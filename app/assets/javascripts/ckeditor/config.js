d47c336e7b925712883cbf94c96a8c8ea2f289b6
adasbasi <dasbasi.ali@gmail.com>
2017-10-27 19:29:42 +0000

    ckeditor

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,4 +1,14 @@
-CKEDITOR.editorConfig = function (config) {
+if (typeof(CKEDITOR) != 'undefined') {
+            CKEDITOR.editorConfig = function( config )
+            {
 	   config.extraPlugins = 'youtube';
+           config.width = '100%'; 
+	config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
+  config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
+  config.filebrowserImageUploadUrl = "/ckeditor/pictures";	
+config.enterMode = CKEDITOR.ENTER_BR;
+   config.shiftEnterMode = CKEDITOR.ENTER_BR;
+   config.autoParagraph = false;
+  }
 }
 
