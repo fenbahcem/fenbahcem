cbfe66621ccc72cc97dab1af6749e26525cd6995
adasbasi <dasbasi.ali@gmail.com>
2017-11-21 16:05:47 +0200

    fotogaleri lightgallery düzenine geçildi.

--- app/models/admin/proje.rb
+++ app/models/admin/proje.rb
@@ -4,18 +4,20 @@ class Admin::Proje < ApplicationRecord
 	validates_attachment :ek, content_type: {
 		content_type: [
 			"application/vnd.ms-excel",
-			"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
 			"application/pdf",
 			"application/msword",
 			"application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+      "application/vnd.openxmlformats-officedocument.presentationml.presentation",
 			"text/plain",
 			"image/png",
 			"image/jpg",
 			"image/jpeg",
 			"image/gif",
+			"text/html",
 			"application/zip",
-			"application/rar"
-
+			"application/x-zip",
+      "application/x-zip-compressed"
 		]
 	}
 end
